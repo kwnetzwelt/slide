3e27a06d8a656b103accf90636a80bc98c07e65a
Kai Wegner <kai@edurino.com>
2023-03-06 21:03:49 +0100

    Initial Commit

---
+++ slide/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  slide
+//
+//  Created by Kai Wegner on 06.03.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
